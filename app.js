f3ba536dd4833d7adcfa9e72978b782b5f19582d
Mauricio Lombard <mlombard82@gmail.com>
2016-11-05 18:11:08 +0000

    Add a better header/title. Make campgrounds display in a grid

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set("view engine", "ejs");
     var campgrounds = [
         {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4082/4857023755_7e2be75031.jpg"},
         {name: "Granite hill", image: "https://farm2.staticflickr.com/1266/973330600_c1360f7cd3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2512/5733464781_8787e851b0.jpg"},
+        {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4082/4857023755_7e2be75031.jpg"},
+        {name: "Granite hill", image: "https://farm2.staticflickr.com/1266/973330600_c1360f7cd3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2512/5733464781_8787e851b0.jpg"},
+        {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4082/4857023755_7e2be75031.jpg"},
+        {name: "Granite hill", image: "https://farm2.staticflickr.com/1266/973330600_c1360f7cd3.jpg"},
         {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2512/5733464781_8787e851b0.jpg"}
     ]
 
