940e75bc4a6a6e4f19a748be40da5b1c4bab94a6
Mauricio Lombard <mlombard82@gmail.com>
2016-11-22 00:49:42 +0000

    Add logout route. Prevent user from adding a comment if not signed in. Add links to navbar

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.get("/campgrounds/:id", function(req, res){
 // COMMENTS ROUTES
 // ==============
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     //find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -98,7 +98,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     //lookup campground using ID
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -155,6 +155,18 @@ app.post("/login", passport.authenticate("local",
     }), function(req, res){
 });
 
+//logic route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn (req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server has Started!");
