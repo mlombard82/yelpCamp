ce04c8f91620055ba2806bbc5b022ce398e23319
Mauricio Lombard <mlombard82@gmail.com>
2016-12-02 13:46:07 +0000

    fix small bug.Had to click register button twice to see flash message

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,9 @@ router.post("/register", function(req, res){
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             req.flash("error", err.message);
-            return res.render("register");
+            return res.redirect("/register");
         }
+        
         passport.authenticate("local")(req, res, function(){
             req.flash("success", "Welcome to YelpCamp " + user.username);
             res.redirect("/campgrounds"); 
