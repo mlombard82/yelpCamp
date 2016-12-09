d30e936802aa3ebb500f81b94515d41ba7f2f575
Mauricio Lombard <mlombard82@gmail.com>
2016-12-09 09:57:41 +0000

    use of env variables. connects to different databases regarding development or production. env variables allow information to be hidden/not exposed

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://mau:pass@ds127998.mlab.com:27998/yelpcammau");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url);
  
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
